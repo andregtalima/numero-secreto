13c0d6de5edd44782f276cec1ed0798518c91601
Andre Fabiano A Lima <andregtalima@gmail.com>
2024-07-31 07:45:09 -0300

    Deixar o jogo mais fácil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -13,8 +13,6 @@ function exibirMensagemInicial() {
   exibirTextoNaTela("h1", "Jogo do número secreto");
   exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 }
-
-exibirMensagemInicial();
 
 function verificarChute() {
   let chute = document.querySelector("input").value;
